700c0addef568cfb1affa2df6c84daa643a3a22b
Visothipong RoTH <administrator@VisothipongMacBook.local>
2022-09-02 13:47:39 +0700

    finish

---
+++ src/components/SinglePost.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="post">
+    <h3>{{ post.title }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["post"],
+  setup(props) {},
+};
+</script>
+
+<style>
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,27 @@
-<script setup>
-</script>
 
 <template>
-  <main>
-    <h1>home</h1>
-  </main>
+  <div class="home">
+    <h1>Home</h1>
+    <Post :posts="post" />
+  </div>
 </template>
+
+  <script>
+import { ref } from "vue";
+import Post from "../components/Post.vue";
+export default {
+  components: { Post },
+  setup() {
+    const post = ref([
+      {
+        title: "welcome to the Pong blog",
+        body: "heh There thanks for visiting us",
+        id: 1,
+      },
+      { title: "Top 5 css tips", body: "Learn css by created", id: 2 },
+    ]);
+    return { post };
+  },
+  components: { Post },
+};
+</script>
